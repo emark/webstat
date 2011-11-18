6d9dda01a1c6375b2d3d0b8ef071161059069a43
Sviridenko Maxim <sviridenko.m@web2buy.ru>
2011-11-18 14:57:00 +0800

    clear pstat

--- scr/pstat.pl
+++ scr/pstat.pl
@@ -2,16 +2,13 @@
 use strict;
 use CGI;
 use DBI;
+require '../sys/syspkg.pl';
 
-#Определение параметров БД
-my $dbhost='mysql0.locum.ru';
-my $dbase='locumtes_web2b58';
-my $user='locumtes_web2b58';
-my $pass='qJ8O1JSg';
-my $SQL='';
-my $dbh=DBI->connect("DBI:mysql:database=$dbase;host=$dbhost",$user,$pass) || die "Can't connect to database: $dbase at $dbhost";
+my $dbconf='../db.conf';
+my $dbh=DBI->connect(&Syspkg::DBconf($dbconf));
 my $sth=undef;
-#Инициализация переменных
+my $SQL='';
+#Variables init
 my $query=new CGI;
 my $variant=$query->param('variant');
 my $host=$ENV{'HTTP_HOST'};
@@ -23,7 +20,7 @@ if($url)
     #Удаление http://имя_хоста из URL
     $url=~s/http:\/\/$host//;
 
-    $SQL="INSERT INTO $dbase.POSTSTAT(URL,DATE,IP,ANSWER) VALUES('$url',NOW(),'$ip',$variant)";
+    $SQL="INSERT INTO POSTSTAT(URL,DATE,IP,ANSWER) VALUES('$url',NOW(),'$ip',$variant)";
     $sth=$dbh->prepare($SQL);
     $sth->execute();
     print $query->header();
@@ -32,4 +29,3 @@ else
 {
     print $query->redirect('http://'.$ENV{'HTTP_HOST'});
 }
-#print '<PRE>';foreach my $key(keys %ENV){print $key."\t$ENV{$key}\n";};#print $SQL;
